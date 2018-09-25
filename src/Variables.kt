2448580c6514b26112023bd939eca507c9ce302e
Arif Hussain <hussain.arif33@gmail.com>
2018-09-25 00:31:33 -0400

    Variables

---
+++ src/Variables.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    val name: String = "Curious George"
+    var isAwesome = true
+
+    println("Is " + name + " awesome? The answer is: " + isAwesome + "!")
+
+    var a = 3
+    var b = 6
+    println(a + b)
+
+    val doubleNum = 123.45
+    val floatNum = 123.45f
+    val longNum = 1234567890987L //64 bit
+    val aDouble = a.toString()
+
+    var hero: String
+    hero = "batman"
+    hero = "superman"
+    println(hero)
+}
